fcfa4cf15ce0bd84eaee572ddebbf80495a6df7e
farooq-azam-khan <khanazamfarooq99@gmail.com>
2023-04-22 20:52:59 -0400

    get user + get game (in progress)

---
+++ get_game.py
@@ -0,0 +1,28 @@
+import requests as r
+from dotenv import load_dotenv
+import os
+
+from pprint import pprint 
+import argparse 
+
+load_dotenv()
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+grant_type = 'client_credentials'
+access_token = os.getenv('ACCESS_TOKEN') 
+parser = argparse.ArgumentParser(prog='Twitch API CLI (User only)', 
+        description='provide a username and you will get data about that twitch user')
+parser.add_argument('username')
+
+top_game_headers = {'Authorization': f'Bearer {access_token}', 
+                    'Client-Id': client_id }
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    params = {'login': args.username}
+    res = r.get(f'https://api.twitch.tv/helix/users',
+                        params, 
+                        headers=top_game_headers)
+    pprint(res.json())
+
